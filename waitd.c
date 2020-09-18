441e1e1e58d7598d962935abe2ae54aaf03c5423
dk949 <56653556+dk949@users.noreply.github.com>
2020-09-19 02:43:19 +0300

    Refactored everything to be more modulear. Main loop still needs som work. TODO: arguments and shell script execution.

--- waitd.c
+++ waitd.c
@@ -12,10 +12,6 @@
 #define GOT_NO_INPUT 1
 #define GOT_INPUT 2
 
-int getGlob(glob_t *glob_result) {
-    return glob("/dev/input/event[0-9]*", GLOB_ERR | GLOB_NOSORT | GLOB_NOESCAPE, NULL, glob_result);
-}
-
 void waitFunc(int f) {
     static int which = 0;
     if (which != f) {
@@ -28,38 +24,38 @@ void waitFunc(int f) {
     }
 }
 
-int main(int argc, char *argv[]) {
-    int *fileDescriptors, ret, i;
-    glob_t glob_result;
-
+void getGlob(glob_t *glob_result) {
+    int ret = glob("/dev/input/event[0-9]*", GLOB_ERR | GLOB_NOSORT | GLOB_NOESCAPE, NULL, glob_result);
 
     /* getGlob will return a non 0 value if glob() fails
      * This will most likely be due to lack of privilege
      * given to the program*/
-    if (getGlob(&glob_result))
+    if (ret)
         err(EXIT_FAILURE, "glob");
+}
 
-
-    /* allocate array for opened file descriptors */
-    fileDescriptors = malloc(sizeof(*fileDescriptors) * (glob_result.gl_pathc + 1));
-
+void getFiles(const glob_t *const glob_result, int *fileDescriptors) {
+    int i;
     /* This is very unlikely to happen, but malloc can fail
      * due to lack of free memory */
     if (fileDescriptors == NULL)
         err(EXIT_FAILURE, "malloc");
 
     /* open devices */
-    for (i = 0; i < glob_result.gl_pathc; i++) {
-        fileDescriptors[i] = open(glob_result.gl_pathv[i], O_RDONLY | O_NONBLOCK);
+    for (i = 0; i < glob_result->gl_pathc; i++) {
+        fileDescriptors[i] = open(glob_result->gl_pathv[i], O_RDONLY | O_NONBLOCK);
         /* open() can fail if the device stopped existing
          * between being discovered with glob() and being
          * accessed here */
         if (fileDescriptors[i] == -1)
-            err(EXIT_FAILURE, "open `%s'", glob_result.gl_pathv[i]);
+            err(EXIT_FAILURE, "open `%s'", glob_result->gl_pathv[i]);
     }
 
     fileDescriptors[i] = -1; /* end of array */
+}
 
+void mainLoop(int *fileDescriptors, float time) {
+    int ret;
     for (;;) {
         char buf[512];
         struct timeval timeout;
@@ -70,7 +66,7 @@ int main(int argc, char *argv[]) {
 
         /* select(2) might alter the fdset, thus freshly set it
            on every iteration */
-        for (i = 0; fileDescriptors[i] != -1; i++) {
+        for (int i = 0; fileDescriptors[i] != -1; i++) {
             FD_SET(fileDescriptors[i], &readFileDescriptors);
             nfds = fileDescriptors[i] >= nfds ? fileDescriptors[i] + 1 : nfds;
 
@@ -85,8 +81,8 @@ int main(int argc, char *argv[]) {
         }
 
         /* same for timeout, 5 seconds here */
-        timeout.tv_sec = 5; /* FIXME */
-        timeout.tv_usec = 0;
+        timeout.tv_sec = (int)time; /* FIXME */
+        timeout.tv_usec = (int)(time * 1000) - (int)time * 1000;
 
         ret = select(nfds, &readFileDescriptors, NULL, NULL, &timeout);
         if (ret == -1)
@@ -97,7 +93,18 @@ int main(int argc, char *argv[]) {
             waitFunc(GOT_INPUT);
         }
     }
+}
 
-    free(fileDescriptors);
+
+int main(int argc, char *argv[]) {
+    int *fileDescriptors;
+    glob_t glob_result;
+    getGlob(&glob_result);
+
+    /* allocate array for opened file descriptors */
+    fileDescriptors = malloc(sizeof(*fileDescriptors) * (glob_result.gl_pathc + 1));
+    getFiles(&glob_result, fileDescriptors);
+
+    mainLoop(fileDescriptors, 5);
     return 0;
 }
