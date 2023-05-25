4b4668393dd5f60ca5eec71984e216539b79d0a7
dk949 <56653556+dk949@users.noreply.github.com>
2023-05-25 21:44:43 +0300

    properly ignoring return of system

--- waitd.cpp
+++ waitd.cpp
@@ -40,6 +40,6 @@ int main(int, char **argv) {
     auto const args = Args::parse(argv);
     while (1) {
         c.wait(args.wait * 1000, args.pollRate * 1000);
-        (void)std::system(args.execOnWait);
+        [[maybe_unused]] int ret = std::system(args.execOnWait);
     }
 }
