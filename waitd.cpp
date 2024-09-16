9b10832347b1bad6363723e3abf611546522742c
dk949 <56653556+dk949@users.noreply.github.com>
2024-09-16 14:31:09 +0100

    use std::chrono for time

--- waitd.cpp
+++ waitd.cpp
@@ -4,6 +4,12 @@
 #include <X11/extensions/scrnsaver.h>
 #include <X11/Xlib.h>
 
+#include <chrono>
+#include <thread>
+
+namespace chr = std::chrono;
+using namespace std::chrono_literals;
+
 struct Context {
     Window win;
     Display *dpy;
@@ -22,12 +28,12 @@ struct Context {
         XCloseDisplay(dpy);
     }
 
-    void wait(unsigned long threshold, long pollRate) {
+    void wait(chr::milliseconds threshold, chr::milliseconds pollRate) {
         XResetScreenSaver(dpy);
         while (1) {
-            msleep(pollRate);
+            std::this_thread::sleep_for(pollRate);
             XScreenSaverQueryInfo(dpy, win, info);
-            unsigned long idle = info->idle;
+            auto const idle = chr::milliseconds(int64_t(info->idle));
             if (idle > threshold) return;
         }
     }
@@ -39,7 +45,7 @@ int main(int, char **argv) {
     c.init();
     auto const args = Args::parse(argv);
     while (1) {
-        c.wait(args.wait * 1000, args.pollRate * 1000);
+        c.wait(chr::milliseconds(int64_t(args.wait * 1000)), chr::milliseconds(int64_t(args.pollRate * 1000)));
         [[maybe_unused]]
         int ret = std::system(args.execOnWait);
     }
